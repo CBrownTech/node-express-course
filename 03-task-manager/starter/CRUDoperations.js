7704f276fdb9ab29fe6fa5b2e2d57a124ea8d34b
CBrownTech <cbrown46@usf.edu>
2023-07-18 06:19:26 -0400

    Lesson for week6 (updated CRUDoperations.js)

--- 03-task-manager/starter/CRUDoperations.js
+++ 03-task-manager/starter/CRUDoperations.js
@@ -1,70 +1,119 @@
-// Read all tasks
+const express = require('express');
+const Joi = require('joi');
+
+const app = express();
+app.use(express.json());
+
+// Define validation schema for task creation
+const taskCreationSchema = Joi.object({
+  title: Joi.string().max(100).required(),
+  description: Joi.string().max(500),
+});
+
+let tasks = []; // Placeholder array to store tasks
+
+// Validate task creation input and create a new task
+app.post('/tasks', (req, res) => {
+  const { error, value } = taskCreationSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const task = createTask(value.title, value.description);
+  return res.status(201).json(task);
+});
+
+// Retrieve all tasks
 app.get('/tasks', (req, res) => {
-    // Retrieve all tasks
-    const tasks = getAllTasks();
-    return res.status(200).json(tasks);
-  });
-  
-  // Read a specific task by ID
-  app.get('/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
-    // Retrieve task by ID
-    const task = getTaskById(taskId);
-    if (!task) {
-      return res.status(404).json({ error: 'Task not found' });
-    }
-    return res.status(200).json(task);
-  });
-  
-  // Update a task
-  app.put('/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
-    // Validate input
-    const { error, value } = taskCreationSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
-    // Update task
-    const updatedTask = updateTask(taskId, value.title, value.description);
-    if (!updatedTask) {
-      return res.status(404).json({ error: 'Task not found' });
-    }
-    return res.status(200).json(updatedTask);
-  });
-  
-  // Delete a task
-  app.delete('/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
-    // Delete task by ID
-    const deletedTask = deleteTask(taskId);
-    if (!deletedTask) {
-      return res.status(404).json({ error: 'Task not found' });
-    }
-    return res.status(204).end();
-  });
-  
-  // Placeholder functions for database operations
-  function getAllTasks() {
-    // Retrieve all tasks from the database
-    // ...
-    return [];
+  const allTasks = getAllTasks();
+  return res.status(200).json(allTasks);
+});
+
+// Retrieve a specific task by ID
+app.get('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  const task = getTaskById(taskId);
+  if (!task) {
+    return res.status(404).json({ error: 'Task not found' });
   }
-  
-  function getTaskById(taskId) {
-    // Retrieve a task by its ID from the database
-    // ...
+  return res.status(200).json(task);
+});
+
+// Update a task
+app.put('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  const { error, value } = taskCreationSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  const updatedTask = updateTask(taskId, value.title, value.description);
+  if (!updatedTask) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+  return res.status(200).json(updatedTask);
+});
+
+// Delete a task
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  const deletedTask = deleteTask(taskId);
+  if (!deletedTask) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+  return res.status(204).end();
+});
+
+// Function to create a new task
+function createTask(title, description) {
+  const task = {
+    id: tasks.length + 1,
+    title,
+    description,
+  };
+  tasks.push(task);
+  return task;
+}
+
+// Function to retrieve all tasks
+function getAllTasks() {
+  return tasks;
+}
+
+// Function to retrieve a task by ID
+function getTaskById(taskId) {
+  return tasks.find(task => task.id === parseInt(taskId));
+}
+
+// Function to update a task
+function updateTask(taskId, title, description) {
+  const task = getTaskById(taskId);
+  if (!task) {
     return null;
   }
-  
-  function updateTask(taskId, title, description) {
-    // Update the task with the provided ID in the database
-    // ...
+  task.title = title;
+  task.description = description;
+  return task;
+}
+
+// Function to delete a task
+function deleteTask(taskId) {
+  const index = tasks.findIndex(task => task.id === parseInt(taskId));
+  if (index === -1) {
     return null;
   }
-  
-  function deleteTask(taskId) {
-    // Delete the task with the provided ID from the database
-    // ...
-    return null;
-  }
-  
+  const deletedTask = tasks[index];
+  tasks.splice(index, 1);
+  return deletedTask;
+}
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  return res.status(500).json({ error: 'Internal server error' });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
